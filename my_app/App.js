5d776b5a4995fd3e0aa2e0dac779ff8d408f493b
Yining Xu <yining.xu@linkplay.com>
2019-02-28 19:49:46 +0800

    简单的主/交叉轴使用、flex、View组件

--- my_app/App.js
+++ my_app/App.js
@@ -7,52 +7,146 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import { Directions } from "react-native-gesture-handler";
 
+/*------------------------简单的主/交叉轴使用-------------------------*/
+/*
+// 简单的主/交叉轴使用
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    margin:30,
+    width:300,
+    height:500,
+    backgroundColor: 'yellow',
+    // 默认主轴方向是column
+    // 设置为横向排列
+    flexDirection: 'row',
+    // 主轴方向
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    // 交叉轴
+    alignItems: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  child1:{
+    width:100,
+    height: 100,
+    backgroundColor: 'green'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  testA: {
-    textAlign: 'center',
-    color: 'red',
-    marginTop: 20,
-    fontSize: 30,
-  },
+  child2:{
+    width:100,
+    height: 100,
+    backgroundColor: 'blue'
+  }
 });
+*/
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+/*------------------------flex-------------------------*/
+/*
+  可以给组件指定flex，flex的值是数字。flex：1表示组件可以撑满所有的剩余空间
+  同时存在多个并列的子组件，flex：1，均分
+  如果这些并列的子组件的flex值不一样，则谁的值更大，谁占据剩余空间的比例就更大（即占据剩余空间的比等于并列组件间flex值的比）
+*/
+/*
+const styles = StyleSheet.create({
+  container: {
+    marginTop:30,
+    flex: 1,
+    backgroundColor: 'yellow'
+  },
+  child1:{
+    marginTop:30,
+    flex: 1,
+    backgroundColor: 'green'
+  },
+  child2:{
+    flex: 2,
+    backgroundColor: 'blue'
+  }
 });
 
 export default class App extends Component{
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        <Text style={styles.testA}>Hallo World!</Text>
+        <View style={styles.child1}></View>
+        <View style={styles.child2}></View>
       </View>
     );
   }
 }
+*/
 
+/*------------------------View标签-------------------------*/
+/*
+  在Web开发中，div是最重要的一个元素，是页面布局的基础
+  在ReactNative开发中，View组件的作用类似div。是最基本的组件。
+  被看做是容器组件
+*/
+/*
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 25,
+    backgroundColor: "#F2F2F2"
+  },
+  flex: {
+    flex: 1
+  },
+  // 多个组件需要设置
+  center: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  item: {
+    flexDirection: "row",
+    backgroundColor: "gray",
+    marginTop: 5,
+    marginLeft: 5,
+    marginRight: 5,
+    height: 80,
+    borderRadius: 5
+  },
 
+  // 给中间的区域设置左右边线
+  lineLeftRight: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: "red"
+  },
+  // 给上半区域设置下边线
+  bottomLine: {
+    borderBottomWidth: 1,
+    borderColor: "white"
+  }
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={[styles.container, styles.flex]}>
+        <View style={styles.item}>
+          <View style={[styles.flex, styles.center]}>
+            <Text>酒店</Text>
+          </View>
+          <View style={[styles.flex, styles.lineLeftRight]}>
+            <View style={[styles.flex, styles.center, styles.bottomLine]}>
+              <Text>海外酒店</Text>
+            </View>
+            <View style={[styles.flex, styles.center]}>
+              <Text>特价酒店</Text>
+            </View>
+          </View>
+          <View style={styles.flex}>
+            <View style={[styles.flex, styles.center, styles.bottomLine]}>
+              <Text>团购</Text>
+            </View>
+            <View style={[styles.flex, styles.center]}>
+              <Text>民宿</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+*/
